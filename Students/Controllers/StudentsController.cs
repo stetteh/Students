60f852d448035aa45e4d1474b0797046959474f5
stetteh <stettehq@gmail.com>
2016-04-06 14:48:02 -0500

    Controller and Initial View

---
+++ Students/Controllers/StudentsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Students.Models;
+
+namespace Students.Controllers
+{
+    public class StudentsController : Controller
+    {
+        // GET: Students
+        public ActionResult Index()
+        {
+            List<Student> students = new List<Student>()
+            {
+                new Student(1, "seth", "quaye", "male", 30),
+                new Student(2, "zach", "ballard", "male", 26),
+            };
+            return View(students);
+        }
+    }
+}
