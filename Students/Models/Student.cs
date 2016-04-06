d911fba204e1076f86b244144b6a530600a7e576
stetteh <stettehq@gmail.com>
2016-04-06 14:37:05 -0500

    model, student class

---
+++ Students/Models/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Students.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+
+        public Student()
+        {
+        }
+
+        public Student(int id, string firstname, string lastname, string gender, int age)
+        {
+            Id = id;
+            FirstName = firstname;
+            LastName = lastname;
+            Gender = gender;
+            Age = age;
+        }
+
+    }
+}
