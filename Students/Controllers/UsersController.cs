26b6ea326bbe10f91a6b255647b7b07145ccbf6e
stetteh <stettehq@gmail.com>
2016-04-06 23:52:13 -0500

    currently not able to print cookie names

---
+++ Students/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Students.Models;
+
+namespace Students.Controllers
+{
+    public class UsersController : Controller
+    {
+        
+        [HttpGet]
+        public ActionResult Hello()
+        {
+            if (Request.Cookies["name"] != null)
+            {
+                string name = Request.Cookies["name"].Value;
+
+                return View(name);
+
+            }
+            
+            return View();
+        }
+
+        public ActionResult Hello(User newUser)
+        {
+            HttpCookie aCookie = new HttpCookie("name");
+            aCookie.Value = newUser.Name;
+            Response.Cookies.Add(aCookie);
+
+            return RedirectToAction("Hello");
+        }
+    }
+}
---
+++ Students/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Students.Models
+{
+    public class User
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
